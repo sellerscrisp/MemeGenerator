a78829770a1d5bd04b854016d6fc5771d8dd3ee2
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-06 16:09:28 -0500

    Finished functionality (remove function is buggy)

--- src/app.js
+++ src/app.js
@@ -1,23 +1,79 @@
-function createMeme() {
-    // ...
+function createMeme(memeImg, topText, bottomText, imgUrl) {
+    topText.innerText = document.getElementById('top-text').value;
+    topText.setAttribute('class', 'top');
+    memeImg.setAttribute('src', imgUrl);
+    bottomText.innerText = document.getElementById('bottom-text').value;
+    bottomText.setAttribute('class', 'bottom');
 }
 
-function showMeme() {
-    // ...
+function showMeme(memeFigure, memeImg, topText, bottomText) {
+    memeFigure.appendChild(topText);
+    memeFigure.appendChild(memeImg);
+    memeFigure.appendChild(bottomText);
+    memeFigure.setAttribute('class', 'new-meme');
 }
 
-function createRemoveButton() {
-    // ...
+function createRemoveButton(removeBtn, icon) {
+    removeBtn.setAttribute('class', 'remove');
+    icon.setAttribute('class', 'far fa-window-close');
+    removeBtn.appendChild(icon);
 }
 
-function submitMeme() {
-    // ...
+function submitMeme(event, form) {
+    event.preventDefault();
+
+    let memeWrapper = document.getElementById('meme-wrapper');
+    let memeDiv = document.createElement('div');
+    let memeFigure = document.createElement('figure');
+
+    let icon = document.createElement('a');
+    let removeBtn = document.createElement('a');
+
+    let topText = document.createElement('figcaption');
+    let bottomText = document.createElement('figcaption');
+
+    let memeImg = document.createElement('img');
+    let imgUrl = document.getElementById('img-url').value;
+
+    createRemoveButton(removeBtn, icon);
+    createMeme(memeImg, topText, bottomText, imgUrl);
+    showMeme(memeFigure, memeImg, topText, bottomText);
+
+    memeDiv.setAttribute('class', 'meme-div');
+    memeDiv.appendChild(removeBtn);
+    memeDiv.appendChild(memeFigure);
+
+    memeWrapper.appendChild(memeDiv);
+
+    memeWrapper.addEventListener('click', function (event) {
+        if (event.path[2].className === 'remove') {
+            event.path[2].parentNode.remove();
+        }
+    });
+
+    // let activeElement = document.getElementById('submit-button');
+
+    form.reset();
+    // resetElement(activeElement);
 }
 
 window.onload = function () {
     let form = document.getElementById('meme-creator');
 
-    form.addEventListener('clicl', function (event) {
-        submitMeme();
+    form.addEventListener('submit', function (event) {
+        submitMeme(event, form);
     });
 };
+
+//! ? reset the 'memeify' button because it interferes with the remove button functionality
+// function resetButton(event) {
+//   let $event = $(event);
+//   let $original = $event.clone();
+
+//   $event.replaceWith($original);
+// }
+
+// window.onblur = function () {
+//   let activeElement = document.querySelector('button');
+//   activeElement.blur();
+// }
